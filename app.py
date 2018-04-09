943732906241344b7e2fbdf5d33832b878dd9352
Hüseyin Yiğit <yigit.hsyn@gmail.com>
2018-04-09 06:57:41 +0200

    Eval versioning.

--- app.py
+++ app.py
@@ -29,11 +29,16 @@ def homepage():
 
 
 modules = [
-    {"id": "const", "name": "Physical constants", "modules": [
+    {"id": "const", "name": "Physical constants", "items": [
         {"id": "c0", "name": "Light in vacuum", "symbol": "c_0", "args": [], "value": eval("constants.c")}
     ]},
-    {"id": "wg", "name": "Waveguide parameters", "modules": [
-        {"id": "fc", "name": "Cut-off frequncy", "symbol": "f_c", "args": [
+    {"id": "wg", "name": "Waveguide parameters", "items": [
+        {"id": "fc", "name": "Cut-off frequncy", "symbol": "f_c", "versions": [
+            {"id": "1", "name": "TE01 mod dielectric filled waveguide", "args": [
+                {"id": "a", "symbol": "a", "name": "Waveguide width"}
+            ],
+                "formula": "\dfrac{1}{2\pi\sqrt{\mu_0\epsilon_0\epsilon}\sqrt{\dfrac{1}{a}}}"}
+        ], "args": [
             {"id": "a", "name": "Waveguide width", "optional": True, "default": 0},
             {"id": "b", "name": "Waveguide height", "optional": True, "default": 0},
             {"id": "m", "name": "First mode index", "optional": False},
@@ -50,64 +55,23 @@ class EM(Resource):
 
 class EM_Constants(Resource):
   def get(self, sym):
-    module = next(module["modules"]
-                  for module in modules if module["id"] == "const")
-    return next(item for item in module if item["id"] == sym)
+    items = next(module["item"] for module in modules if module["id"] == "const")
+    return next(item for item in items if item["id"] == sym)
+
 
 class EM_Waveguide(Resource):
   def get(self, sym):
-    module = next(module["modules"]
-                  for module in modules if module["id"] == "wg")
-    submodule = next(item for item in module if item["id"] == sym)
-    for arg in submodule["args"]:
+    items = next(module["modules"] for module in modules if module["id"] == "wg")
+    item = next(item for item in items if item["id"] == sym)
+    for arg in item["args"]:
       if arg["id"] not in request.args.keys() and not arg["optional"]:
         return {"error": ""}
-      else: 
+      else:
         return request.args
-    return request.args 
-
-# class TinyDB_Table(Resource):
-#   def get(self, table):
-#     table = tinydbDatabase.table(table)
-#     items = []
-#     for item in table.all():
-#       item["id"] = item.doc_id
-#       items.append(item)
-#     return items
-#   def post(self, table):
-#     table = tinydbDatabase.table(table)
-#     return table.insert(request.json)
-#   def delete(self, table):
-#     tinydbDatabase.purge_table(table)
-#     return {}
-
-# class TinyDB_Item(Resource):
-#   def get(self, table, doc_id):
-#     table = tinydbDatabase.table(table)
-#     try:
-#       item = table.get(doc_id=int(doc_id))
-#       item["id"] = doc_id
-#       return item
-#     except Exception:
-#       return {"error": {"type": "api", "msg": "TinyDB_Item: GET\n" + str(traceback.format_exc())}}
-#   def post(self, table, doc_id):
-#     table = tinydbDatabase.table(table)
-#     try:
-#       table.update(request.json, doc_ids=[int(doc_id)])
-#       return {}
-#     except Exception:
-#       return {"error": {"type": "api", "msg": "TinyDB_Item: POST\n" + str(traceback.format_exc())}}
-#   def delete(self, table, doc_id):
-#     table = tinydbDatabase.table(table)
-#     try:
-#       table.remove(doc_ids=[int(doc_id)])
-#       return {}
-#     except Exception:
-#       return {"error": {"type": "api", "msg": "TinyDB_Item: DEL\n" + str(traceback.format_exc())}}
+    return request.args
 
 
 api.add_resource(EM, '/em')
 api.add_resource(EM_Constants, '/em/const/<sym>')
 api.add_resource(EM_Waveguide, '/em/wg/<sym>')
-# api.add_resource(TinyDB_Item, '/tinydb/<table>/<doc_id>')
 app.run(port=5001, debug=True)
