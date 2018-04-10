e7c4c554bf9ca1447385d7a8b1c9d8047437f446
Hüseyin YİĞİT <yigit.hsyn@outlook.com>
2018-04-10 16:09:59 +0300

    Test: Evalution with input args tested.

--- app.py
+++ app.py
@@ -2,12 +2,14 @@
 import os
 import traceback
 from datetime import datetime
+import json
 
 # Installed modules
 from flask import Flask, request, redirect, send_from_directory, send_file
 from flask_restful import Resource, Api, reqparse
 from flask_cors import CORS
-from scipy import constants, sqrt
+from scipy.constants import c, pi
+from scipy import sqrt
 
 # run flask app
 app = Flask(__name__, static_folder='test')
@@ -15,9 +17,7 @@ CORS(app)
 api = Api(app)
 parser = reqparse.RequestParser()
 
-# serve index.html
-
-
+# serve static files
 @app.route('/<path:path>')
 def static_proxy(path):
     return send_from_directory("test", path)
@@ -28,23 +28,8 @@ def homepage():
     return send_from_directory("test", "index.html")
 
 
-modules = [
-    {"id": "const", "name": "Physical constants", "items": [
-        {"id": "c0", "name": "Light in vacuum", "symbol": "c_0", "args": [], "value": eval("constants.c")}
-    ]},
-    {"id": "wg", "name": "Waveguide parameters", "items": [
-        {"id": "fc", "name": "Cut-off frequency", "symbol": "f_c", "versions": [
-            {"id": "1", "name": "TE01 mod dielectric filled waveguide", "$height": 55, "args": [
-                {"id": "er", "symbol": "\\epsilon_r", "cast":"float", "name": "Dielectric coefficient of substrate"},
-                {"id": "a", "symbol": "a", "cast": "float", "name": "Waveguide width"}
-            ],
-                "formula": "\\dfrac{1}{2\\pi\\sqrt{\\mu_0\\epsilon_0\\epsilon_r}}\\sqrt{\\dfrac{1}{a}}" +
-                "= \\dfrac{c_0}{2\\pi\\sqrt{\\epsilon_r\\times a}}",
-                "eval": "lambda args: constants.c/(2*constants.pi*sqrt(args[0]*args[1]))"
-            }
-        ]}
-    ]}
-]
+with open('modules.json', encoding="utf-8") as data_file:
+    modules = json.load(data_file)
 
 
 class EM(Resource):
@@ -55,23 +40,21 @@ class EM(Resource):
 class EM_Constants(Resource):
     def get(self, sym):
         module = next(module["items"] for module in modules if module["id"] == "const")
-        return next(item for item in module if item["id"] == sym)
+        return next(eval(item["value"]) for item in module if item["id"] == sym)
 
 
 class EM_Waveguide(Resource):
-    def get(self, sym):
+    def get(self, sym, ver):
         module = next(module["items"] for module in modules if module["id"] == "wg")
         item = next(item for item in module if item["id"] == sym)
         largs = []
-        for arg in item["versions"][0]["args"]:
-          largs.append(eval(arg["cast"])(request.args[arg["id"]]))
-        print(largs)
-        # for arg in request.args:
-        #   print(arg)
-        return eval(item["versions"][0]["eval"])(largs)
+        eval_ver = item["versions"][int(ver)-1]
+        for arg in eval_ver["args"]:
+            largs.append(eval(arg["cast"])(request.args[arg["id"]]))
+        return eval(eval_ver["eval"])(largs)
 
 
 api.add_resource(EM, '/em')
 api.add_resource(EM_Constants, '/em/const/<sym>')
-api.add_resource(EM_Waveguide, '/em/wg/<sym>')
+api.add_resource(EM_Waveguide, '/em/wg/<sym>/<ver>')
 app.run(port=5001)
