e3304657f1dce79a3135727deded603483bbc8c6
Hüseyin YİĞİT <yigit.hsyn@outlook.com>
2018-04-11 14:18:01 +0300

    Some calculations added.

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ import json
 from flask import Flask, request, redirect, send_from_directory, send_file
 from flask_restful import Resource, Api, reqparse
 from flask_cors import CORS
-from scipy.constants import c, pi
+from scipy.constants import c, pi, epsilon_0, mu_0
 from scipy import sqrt
 
 # run flask app
@@ -39,22 +39,22 @@ class EM(Resource):
 
 class EM_Constants(Resource):
     def get(self, sym):
-        module = next(module["items"] for module in modules if module["id"] == "const")
-        return next(eval(item["value"]) for item in module if item["id"] == sym)
+        module = next(module["items"] for module in modules if module["_id"] == "const")
+        return next(eval(item["value"]) for item in module if item["_id"] == sym)
 
 
-class EM_Waveguide(Resource):
-    def get(self, sym, ver):
-        module = next(module["items"] for module in modules if module["id"] == "wg")
-        item = next(item for item in module if item["id"] == sym)
+class EM_Parameters(Resource):
+    def get(self, mod, sym, ver):
+        module = next(module["items"] for module in modules if module["_id"] == mod)
+        item = next(item for item in module if item["_id"] == sym)
         largs = []
         eval_ver = item["versions"][int(ver)-1]
         for arg in eval_ver["args"]:
-            largs.append(eval(arg["cast"])(request.args[arg["id"]]))
+            largs.append(eval(arg["cast"])(request.args[arg["_id"]]))
         return eval(eval_ver["eval"])(largs)
 
 
 api.add_resource(EM, '/em')
 api.add_resource(EM_Constants, '/em/const/<sym>')
-api.add_resource(EM_Waveguide, '/em/wg/<sym>/<ver>')
+api.add_resource(EM_Parameters, '/em/<mod>/<sym>/<ver>')
 app.run(port=5001)
